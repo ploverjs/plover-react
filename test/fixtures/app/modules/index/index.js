9dee647ebbf7adc0115c452ba53f6b611cc8f98c
bencode <bencode@163.com>
2017-11-05 20:58:52 +0800

    add test

--- test/fixtures/app/modules/index/index.js
+++ test/fixtures/app/modules/index/index.js
@@ -1,4 +1,4 @@
 exports.view = ctx => {
   ctx.layout = false;
-  ctx.render({ name: 'hello' });
+  ctx.render({ name: 'hello', state: { name: 'plover-react' } });
 };
--- test/fixtures/app/static/server.js
+++ test/fixtures/app/static/server.js
@@ -1,4 +1,10 @@
-const Hello = ({name}) => {
+const { Provider, connect } = require('react-redux');
+
+
+/* global React */
+
+
+const Hello = ({ name }) => {
   return React.createElement(
     'div',
     {},
@@ -7,7 +13,17 @@ const Hello = ({name}) => {
 };
 
 
-module.exports = {
-  Hello
+const App = ({ store }) => {
+  const C = connect(state => state)(Hello);
+  return React.createElement(
+    Provider,
+    { store },
+    React.createElement(C)
+  );
 };
 
+
+module.exports = {
+  Hello,
+  App
+};
